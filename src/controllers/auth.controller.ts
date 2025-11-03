0cb07a0adc5f5e4dd8ed91aa6cf75f40557e7fa7
amandajcustodio <amandajcustodio@outlook.com>
2025-11-03 08:05:21 -0300

    fix(auth): return companyId on login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express";
 import { AuthService } from "../services/auth.service.js";
+import { UserService } from "../services/user.service.js";
 
 export class AuthController {
   public static async login(req: Request, res: Response): Promise<void> {
     const { corporateEmail, password } = req.body;
     const userRecord = await new AuthService().login(corporateEmail, password);
     const token = await userRecord.user.getIdToken(true);
-    console.log(userRecord)
+    const user = await new UserService().getById(userRecord.user.uid);
 
-    res.send({ token: token, id: userRecord.user.uid, email: userRecord.user.email });
+    res.send({ token: token, id: userRecord.user.uid, email: user.corporateEmail, companyId: user.companyId });
   }
 
   public static async recovery(req: Request, res: Response): Promise<void> {
