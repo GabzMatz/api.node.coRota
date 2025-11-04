3e867cd7f7fbca01b2f4ed83b3ae1bdaed94e4d8
amandajcustodio <amandajcustodio@outlook.com>
2025-11-03 23:03:35 -0300

    refactor: change structure to be able to deploy application

--- src/errors/email-already-exists.error.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorBase } from "./base.error.js";
-
-export class EmailAlreadyExists extends ErrorBase {
-  constructor(message: string = "O e-mail informado já está vinculado a uma conta!") {
-    super(409, message);
-  }
-}
--- src/errors/internal-server.error.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorBase } from "./base.error.js";
-
-export class InternalServerError extends ErrorBase {
-  constructor(message: string = "Erro interno no servidor!") {
-    super(500, message)
-  }
-}
